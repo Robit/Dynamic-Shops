ab46439d9cc94f1145875fb0baaf4821c8554391
RohitM2019 <rohitmittal@sfhs.com>
2019-12-22 21:10:46 -0800

    Added AdjustShopData and CreateShopData

--- src/main/java/io/github/rm2023/dynamicshops/util/AdjustPriceData.java
+++ src/main/java/io/github/rm2023/dynamicshops/util/AdjustPriceData.java
@@ -1,5 +1,15 @@
 package io.github.rm2023.dynamicshops.util;
 
-public class AdjustPriceData {
+import org.spongepowered.api.entity.living.player.Player;
 
+import io.github.rm2023.dynamicshops.shop.Shop;
+
+public class AdjustPriceData {
+    public Player player;
+    public double price;
+    public AdjustPriceData(Player p, double price)
+    {
+	player = p;
+	this.price = price;
+    }
 }
--- src/main/java/io/github/rm2023/dynamicshops/util/CreateShopData.java
+++ src/main/java/io/github/rm2023/dynamicshops/util/CreateShopData.java
@@ -1,6 +1,15 @@
 package io.github.rm2023.dynamicshops.util;
 
+import org.spongepowered.api.entity.living.player.Player;
+
+import io.github.rm2023.dynamicshops.shop.Shop;
+
 public class CreateShopData {
     public Player player;
     public Shop shop;
+    public CreateShopData(Player p, Shop s)
+    {
+	player = p;
+	shop = s;
+    }
 }
